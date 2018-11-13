2d2885da77c2d7ab043a3e593c6571f45d3b8b4e
Toufik <toufik.menad@gmail.com>
2018-11-13 07:53:40 -0500

    in progress

--- restservices/src/main/java/com/canada/recipe/RecipeApplication.java
+++ restservices/src/main/java/com/canada/recipe/RecipeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /*
- * First commit
+ * First commit --reloaded the project
  * a test to make sure the git is set up correctly
  * added moduless
  */
